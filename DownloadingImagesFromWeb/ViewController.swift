3845cbb3edbfc5a811d2b6f1881479fa5106eee9
SergeyGolenko <1989sergiotay@gmail.com>
2020-12-28 03:06:07 +0200

    picture load from web/ URLSession done

--- DownloadingImagesFromWeb/ViewController.swift
+++ DownloadingImagesFromWeb/ViewController.swift
@@ -8,10 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var imageFromWeb: UIImageView!
+    
+    let url = URL(string:"https://upload.wikimedia.org/wikipedia/commons/thumb/a/a1/Registan_Complex_%288145371129%29.jpg/240px-Registan_Complex_%288145371129%29.jpg")
+    
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        let request = NSMutableURLRequest(url:self.url!)
+        let task = URLSession.shared.dataTask(with: request as URLRequest) { (data, response, error) in
+            if error != nil {
+                print("ERROR = \(error)")
+            } else {
+                if let data = data{
+                    if let bachImage = UIImage(data: data){
+                         DispatchQueue.main.async {
+                            self.imageFromWeb.image = bachImage
+                        }
+                     
+                        
+                    }
+                }
+                
+            }
+            
+        }
+        task.resume()
     }
 
 
